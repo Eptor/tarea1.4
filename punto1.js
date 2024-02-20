ccfd96a6c777698062144f580cccced64c57937f
Hector Espinoza <hdespinoza@aliquammed.com>
2024-02-20 09:23:55 -0800

    Se eliminó el callback hell

--- punto1.js
+++ punto1.js
@@ -5,38 +5,32 @@ try {
         throw new Error('N debe ser un entero mayor a cero.');
     }
 
-    // Simulación de una lista de asignaturas
     const asignaturas = [
         { clave: 101, nombre: "Matemáticas", creditos: 4 },
         { clave: 102, nombre: "Programación", creditos: 6 },
         { clave: 103, nombre: "Física", creditos: 5 }
     ];
 
-    // Función para generar fechas aleatorias dentro de un rango
     const generarFechaAleatoria = (inicio, fin) => new Date(inicio.getTime() + Math.random() * (fin.getTime() - inicio.getTime()));
 
-    // Definimos el rango de fechas desde 01/01/2021 hasta 30/06/2023
     const inicio = new Date('2023-08-01');
     const fin = new Date('2024-02-20');
 
-    // Crear un arreglo de tamaño N con objetos simulando registros
     const historial = Array.from({ length: N }, () => {
         const asignatura = asignaturas[Math.floor(Math.random() * asignaturas.length)];
         return {
             clave: asignatura.clave,
-            creditos: Math.floor(Math.random() * (9 - 4) + 4), // Créditos entre 4 y 8
+            creditos: Math.floor(Math.random() * (9 - 4) + 4),
             nombre: asignatura.nombre,
-            calificacion: parseFloat((Math.random() * 100).toFixed(2)), // Calificación entre 0 y 100
+            calificacion: parseFloat((Math.random() * 100).toFixed(2)),
             fecha: generarFechaAleatoria(inicio, fin)
         };
     });
     // console.log(historial)
 
-    // Fecha de corte para los últimos 6 meses desde la fecha actual
     const fechaCorte = new Date();
     fechaCorte.setMonth(fechaCorte.getMonth() - 6);
 
-    // Filtrar y mapear el historial según los criterios
     const resultado = historial
         .filter(({ calificacion, fecha }) => calificacion < 60 && fecha > fechaCorte)
         .map(({ clave, creditos, nombre, calificacion, fecha }) => ({
--- punto4.js
+++ punto4.js
@@ -1,17 +1,8 @@
 const servicio = require("./servicioDatos");
 
-let getDatosOrdenadosPorCallback = function () {
-    servicio.obtenDatosDeInternet(1, 3000, () => {
-        servicio.obtenDatosDeInternet(2, 500, () => {
-            servicio.obtenDatosDeInternet(3, 4000, () => {
-                servicio.obtenDatosDeInternet(4, 700, () => {
-                    servicio.obtenDatosDeInternet(5, 3500, () => {
-                        console.log('Todos los datos han sido obtenidos');
-                    });
-                });
-            });
-        });
-    });
-};
-
-getDatosOrdenadosPorCallback();
+servicio.obtenDatosDeInternet(1, 3000)
+    .then(() => servicio.obtenDatosDeInternet(2, 500))
+    .then(() => servicio.obtenDatosDeInternet(3, 4000))
+    .then(() => servicio.obtenDatosDeInternet(4, 700))
+    .then(() => servicio.obtenDatosDeInternet(5, 3500))
+    .then(() => console.log('Todos los datos han sido obtenidos usando Promesas y .then()'));
